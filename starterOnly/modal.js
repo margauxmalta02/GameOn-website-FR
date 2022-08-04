2080b67ae6a7957b5e53ba361cf9275d14e54029
margaux maltaverne <margauxmaltaverne@gmail.com>
2022-08-04 18:57:23 +0200

    changes

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -12,12 +12,21 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+
+//modification div modal form
+const validate = document.querySelector(".btnsubmit");
+validate.addEventListener(,validateform);
+function validateform(){
+  console.log("coucou");
 }
 
 
